fefb4df342c92242654be9b7a49b02fa898374a3
OkaGouhei <okagouhei@okagoutairanoMacBook-puro.local>
2018-07-11 11:26:47 +0900

    correct

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
 
-  resources :prototypes, only: [:index, :new, :create, :show, :destroy]
+  resources :prototypes, only: [:index, :new, :create, :show, :destroy]do
+    resources :comments, only: [:create]
+  end
   #destory を追加(岡)
   resources :users, only: [:show, :edit, :update]
 
-  resources :prototypes do
-    resources :comments, only: [:create]
-  end
 end
