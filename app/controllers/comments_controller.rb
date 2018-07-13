02398dd60cd4186a4a58a0d1bd805c665a329f4e
OkaGouhei <okagouhei@okagoutairanoMacBook-puro.local>
2018-07-13 12:30:37 +0900

    Changed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,6 +12,20 @@ class CommentsController < ApplicationController
     redirect_to prototype_path(params[:prototype_id])
   end
 
+  def edit
+    @comment = Comment.find(params[:id])
+    @prototype = Prototype.find(params[:prototype_id])
+    @comments = @prototype.comments.includes(:user)
+  end
+
+  def update
+    comment = Comment.find(params[:id])
+    if comment.user_id == current_user.id
+      comment.update(comment_params)
+    end
+    redirect_to prototype_path(params[:prototype_id])
+  end
+
   private
   def comment_params
     params.permit(:text, :prototype_id)
